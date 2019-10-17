e61fa1f76def4382eaa51e50617d298df81b2616
mayumi <dffxw608@gmail.com>
2019-10-17 12:33:54 +0000

    first commit

--- database/migrations/2019_10_10_141239_create_profile_histories_table.php
+++ database/migrations/2019_10_10_141239_create_profile_histories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProfile_historiesTable extends Migration
+class CreateProfileshistoriesTable extends Migration
 {
     /**
      * Run the migrations.
